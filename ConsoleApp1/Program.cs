5dea2226640bf0f10725f30058016365baeeea0a
Ruthikreddy <ruthikreddy15@gmail.com>
2020-09-27 20:28:19 +0530

    FirstGOD

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LearningProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int EmpPresent = 1;
+            Random randomnum = new Random();
+            int EmpCheck = randomnum.Next(0, 2);
+            if (EmpCheck == EmpPresent)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
